bdb611144709a01e445a45f25c12b907ef288f58
Embedead <delepis3@gmail.com>
2021-06-26 15:00:32 +0300

    Reinstalled dep without -dev suffix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import "./App.css";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
 
 function App() {
   return (
-    <Homepage />
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/">
-    //       <Homepage />
-    //     </Route>
-    //     <Route exact path="/favorites">
-    //       here be dragons
-    //     </Route>
-    //   </Switch>
-    // </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route exact path="/favorites">
+          here be dragons
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
